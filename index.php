467ca44a37c45a9e2fc4d5745e12f79fe6eefc3a
Wojciech <przyluskiwojciech@gmail.com>
2024-12-17 11:10:19 +0100

    chore: initial setup for project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Piekarnia</title>
+</head>
+<body>
+  <h1>Zamówienie online</h1>
+</body>
+</html>
