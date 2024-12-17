4ab48a9f1deac4d8831f480e79a2bf91633ff2d8
Wojciech <przyluskiwojciech@gmail.com>
2024-12-17 11:22:37 +0100

    feat: create simple form

--- index.php
+++ index.php
@@ -7,5 +7,16 @@
 </head>
 <body>
   <h1>Zamówienie online</h1>
+
+  <form action="order.php" method="post">
+    Ile pączków (0.99 PLN/szt);
+    <input type="text" name="paczkow"/>
+      <br><br>
+    Ile grzebieni (1.29 PLN/szt):
+    <input type="text" name="grzebieni"/>
+      <br><br>
+    <input type="submit" value="Wyślij zamówienie">
+  </form>
+
 </body>
 </html>
